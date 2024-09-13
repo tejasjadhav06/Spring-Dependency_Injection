b611cc7aec365e3c3e3040f21b76deaccbc08de3
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-13 11:25:11 +0530

    Injecting Dependency using @Component Annotation

---
+++ Spring_DI/src/main/java/com/example/Spring_DI/Dev.java
@@ -0,0 +1,11 @@
+package com.example.Spring_DI;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+	public void build() {
+		System.out.println("Working on Awesome Project");
+	}
+}
--- Spring_DI/src/main/java/com/example/Spring_DI/SpringDiApplication.java
+++ Spring_DI/src/main/java/com/example/Spring_DI/SpringDiApplication.java
@@ -2,12 +2,17 @@ package com.example.Spring_DI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SpringDiApplication.class, args);
+		Dev obj = context.getBean(Dev.class);
+		
+		obj.build();
+	
 	}
 
 }
